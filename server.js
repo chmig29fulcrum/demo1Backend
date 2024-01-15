22068c5e80c9b9735373b05e2eaed21988eedf56
CH <chmig29fulcrum@gmail.com>
2024-01-15 19:15:21 +0400

    version 1.0.6

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ const nodeEnv = process.env.NODE_ENV.trim();
 const portVar = `PORT_${nodeEnv.toUpperCase()}`;
 const dbVar = `DATABASE_${nodeEnv.toUpperCase()}`;
 console.log(portVar);
-console.log(process.env.portVar);
-const port = process.env.portVar || 4001;
+console.log(process.env[portVar]);
+const port = process.env[portVar] || 4001;
 
 const app = require('./app');
 
-const DB = process.env.dbVar;
+const DB = process.env[dbVar];
 
 mongoose
   .connect(DB, {
